163f4570e2535e52f80d12c15d371e05061e8454
mujurin1 <mujurin25@yahoo.co.jp>
2025-12-23 20:21:32 +0900

    画像の追加・削除

--- src/util/readAssets.ts
+++ src/util/readAssets.ts
@@ -10,27 +10,42 @@ export interface PreviewInfo {
 }
 
 export const JigsawAssets = [
-  { name: "zunda", title: "ずんだ" },
-  { name: "roulette", title: "ルーレット" },
-  { name: "okonomi", title: "お好み焼き" },
-  { name: "tibi", title: "ちび" },
+  { name: "komadori.png", title: "コマドリ" },
+  { name: "simaenaga.jpg", title: "シマエナガ" },
+  { name: "christmas-dog.jpg", title: "クリスマス犬" },
+  { name: "duck.jpg", title: "カモ" },
+  { name: "inko.jpg", title: "インコ" },
+  { name: "horse.jpg", title: "馬" },
+  { name: "tibi.jpg", title: "ちび" },
 
-  { name: "galaxy", title: "銀河" },
-  { name: "moon", title: "月" },
-  { name: "tree", title: "ツリー" },
-  { name: "plum", title: "うめ" },
-  { name: "horse", title: "馬" },
-  { name: "city", title: "街" },
-  { name: "christmas-dog", title: "クリスマス犬" },
-  { name: "cookie-doll", title: "クッキー人形" },
-  { name: "duck", title: "カモ" },
+  { name: "plum.jpg", title: "うめ" },
+  { name: "fog.jpg", title: "霧" },
+  { name: "world-tree.jpg", title: "世界樹" },
+
+  { name: "tree.png", title: "ツリー" },
+  { name: "cave.png", title: "洞窟" },
+  { name: "landscape-painting.png", title: "風景画" },
+
+  { name: "city.jpg", title: "街" },
+  { name: "daiseidou.png", title: "大聖堂" },
+  { name: "steel-bridge.png", title: "鉄橋" },
+
+  { name: "moon.jpg", title: "月" },
+  { name: "galaxy.jpg", title: "銀河" },
+
+  { name: "kotoyomi-nia.png", title: "琴詠ニア" },
+
+  { name: "cookie-doll.jpg", title: "クッキー人形" },
+  { name: "okonomi.jpg", title: "お好み焼き" },
+
+  { name: "e.jpg", title: "えっ" },
 ] as const;
 
 export function readAssets(scene: g.Scene): PreviewInfo[] {
   return JigsawAssets
     .map(x => ({
       title: x.title,
-      imageAsset: scene.asset.getImage(`${AssetInfo.ASSET_DIR}/${x.name}.jpg`),
+      imageAsset: scene.asset.getImage(`${AssetInfo.ASSET_DIR}/${x.name}`),
     }));
   // const pictureInfo =
   //   scene.asset.getText(AssetInfo.SETTING_TXT).data.split("\r\n")
