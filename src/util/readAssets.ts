a41d840fd77f3d27178b66b8b4a51e151c7cb00a
mujurin1 <mujurin25@yahoo.co.jp>
2025-12-23 15:38:01 +0900

    画像の変更

--- src/util/readAssets.ts
+++ src/util/readAssets.ts
@@ -10,26 +10,21 @@ export interface PreviewInfo {
 }
 
 export const JigsawAssets = [
-  { name: "fox", title: "狐" },
-  { name: "duck", title: "アヒルの家族" },
-  { name: "excavators", title: "自走式掘削機" },
-  { name: "cake", title: "ブルーベリーケーキ" },
-  { name: "latte-art", title: "ラテアート" },
-  { name: "firewood", title: "薪" },
-  { name: "frangipani", title: "プルメリア" },
-  { name: "beach", title: "波のトンネル" },
-  { name: "antelope", title: "レイヨウの渓谷" },
-  { name: "paraglider", title: "パラグライダー" },
-  { name: "skyscrapers", title: "香港の夜景" },
-  { name: "supreme", title: "ライヒ裁判所" },
-  { name: "noodle-soup", title: "ラーメン" },
-  { name: "audience", title: "スタジアム" },
-  { name: "japan", title: "八坂神社" },
-  { name: "color-fan", title: "色見本" },
-  { name: "watch", title: "腕時計" },
-  { name: "cockpit", title: "コックピット" },
-  { name: "living-room", title: "リビング" },
-  { name: "architecture", title: "高速道路" },
+  { name: "zunda", title: "ずんだ" },
+  { name: "roulette", title: "ルーレット" },
+  { name: "okonomi", title: "お好み焼き" },
+  { name: "roulette", title: "ルーレット" },
+  { name: "tibi", title: "ちび" },
+
+  { name: "galaxy", title: "銀河" },
+  { name: "moon", title: "月" },
+  { name: "tree", title: "ツリー" },
+  { name: "plum", title: "うめ" },
+  { name: "horse", title: "馬" },
+  { name: "city", title: "街" },
+  { name: "christmas-dog", title: "クリスマス犬" },
+  { name: "cookie-doll", title: "クッキー人形" },
+  { name: "duck", title: "カモ" },
 ] as const;
 
 export function readAssets(scene: g.Scene): PreviewInfo[] {
